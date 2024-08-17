0071890748afd15dbdb2965f006a72b9bc3b82a0
muhammedyasin1 <muhammedyasin697@gmail.com>
2024-08-17 17:48:18 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,24 @@
+const display= document.getElementById('display');
+
+ function appendtodisplay(input){
+    display.value += input;
+    
+ }
+
+ function cleardisplay(){
+    display.value=" ";
+ }
+
+ function calculate(){
+    try{
+    display.value = eval(display.value);
+    }
+    catch(error){
+        display.value="Error";
+    }
+   
+ }
+
+ function back(){
+    display.value=display.value.slice(0,-1)
+}
